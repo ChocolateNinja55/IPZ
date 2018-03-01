5d9e477063ee7afc8c099b6ad54429341fe9d819
Filip R <filip.ruchel@gmail.com>
2018-03-01 14:18:54 +0100

    Dodany_openCV

--- IPZ/Source.cpp
+++ IPZ/Source.cpp
@@ -1,6 +1,9 @@
 #include "MyForm.h"
+#include <opencv2/opencv.hpp> //Include file for every supported OpenCV function
+#include <iostream>
 using namespace System;
 using namespace System::Windows::Forms;
+//using namespace std;
 
 [STAThreadAttribute]
 int main(array < System::String ^> ^args)
@@ -8,8 +11,16 @@ int main(array < System::String ^> ^args)
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	IPZ::MyForm forma;
+	cv::Mat img = cv::imread("lena10.jpg");
+	if (img.empty())
+		std::cout << "kurwa no elo";
+	cv::namedWindow("Example1", cv::WINDOW_AUTOSIZE);
+	cv::imshow("Example1", img);
+	cv::waitKey(0);
+	cv::destroyWindow("Example1");
 
 	Application::Run(%forma);
+
 
 	return(0);
 }
